62447493b0bb81e426ba312148dad1c796e24cc9
omegamaster86 <masya868686@gmail.com>
2025-07-12 14:36:28 +0900

    新しいAttackLogコンポーネントを作成し、ヘッダーを表示する機能を追加しました。

---
+++ app/attacklog/page.tsx
@@ -0,0 +1,11 @@
+import { Header } from "./header";
+
+const AttackLog = () => {
+  return (
+    <div className="max-w-screen-lg px-16 py-8 mx-auto">
+      <Header />
+    </div>
+  )
+}
+
+export default AttackLog;
