815522d427f791d91538a88bb5ef32b6e993a9de
omegamaster86 <masya868686@gmail.com>
2025-07-12 14:24:00 +0900

    update

---
+++ app/components/auth-button.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { createClient } from "@/lib/supabase/server";
+import { LogoutButton } from "./logout-button";
+
+export async function AuthButton() {
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  return user ? (
+    <div className="flex items-center gap-4 ">
+      Hey, {user.email}!
+      <div className="border-2 border-solid">
+        <LogoutButton />
+      </div>
+    </div>
+  ) : (
+    <div className="flex gap-2">
+      <Button asChild size="sm" variant={"outline"}>
+        <Link href="/auth/login">Sign in</Link>
+      </Button>
+      <Button asChild size="sm" variant={"default"}>
+        <Link href="/auth/sign-up">Sign up</Link>
+      </Button>
+    </div>
+  );
+}
