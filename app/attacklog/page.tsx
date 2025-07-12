1fc7b0d44951c215b3d2a078ef2616c44bd758f8
omegamaster86 <masya868686@gmail.com>
2025-07-12 15:25:42 +0900

    AttackLogコンポーネントを更新し、CompanyInfoをCompanyInformationに変更し、Supabaseから取得した企業情報をpropsとして渡すようにしました。

--- app/attacklog/page.tsx
+++ app/attacklog/page.tsx
@@ -1,7 +1,7 @@
 import { Container, Grid, GridCol, Stack } from "@mantine/core";
 import { AttackHistory } from "./attack-history";
 import { AttackLogForm } from "./attack-log-form";
-import { CompanyInfo } from "./company-info";
+import { CompanyInformation } from "./company-info";
 import { Header } from "./header";
 import { PersonInfo } from "./person-info";
 import { createClient } from "@/lib/supabase/server";
@@ -9,6 +9,7 @@ import { createClient } from "@/lib/supabase/server";
 const AttackLog = async () => {
 	const supabase = await createClient();
 	const { data: callingResult } = await supabase.rpc("get_calling_result", {});
+	const { data: companies } = await supabase.rpc("get_company_information", {});
 
 	return (
 		<Container size="xl" py="lg">
@@ -17,7 +18,7 @@ const AttackLog = async () => {
 			<Grid mt="xl" gutter="lg">
 				<GridCol span={{ base: 12, lg: 6 }}>
 					<Stack gap="lg">
-						<CompanyInfo />
+						<CompanyInformation companies={companies} />
 						<PersonInfo />
 						<AttackLogForm callingResult={callingResult || []} />
 					</Stack>
