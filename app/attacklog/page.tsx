f1ab9e0b5977089fe963d038ab662a2ba8c3dd3f
omegamaster86 <masya868686@gmail.com>
2025-07-12 14:57:53 +0900

    AttackLogコンポーネントを更新し、MantineのContainer、Grid、Stackを使用してレイアウトを改善しました。会社情報、個人情報、攻撃ログフォーム、攻撃履歴を表示する新しい構造を追加しました。

--- app/attacklog/page.tsx
+++ app/attacklog/page.tsx
@@ -1,11 +1,30 @@
+import { Container, Grid, GridCol, Stack } from "@mantine/core";
+import { AttackHistory } from "./attack-history";
+import { AttackLogForm } from "./attack-log-form";
+import { CompanyInfo } from "./company-info";
 import { Header } from "./header";
+import { PersonInfo } from "./person-info";
 
 const AttackLog = () => {
-  return (
-    <div className="max-w-screen-lg px-16 py-8 mx-auto">
-      <Header />
-    </div>
-  )
-}
+	return (
+		<Container size="xl" py="lg">
+			<Header />
+
+			<Grid mt="xl" gutter="lg">
+				<GridCol span={{ base: 12, lg: 6 }}>
+					<Stack gap="lg">
+						<CompanyInfo />
+						<PersonInfo />
+						<AttackLogForm />
+					</Stack>
+				</GridCol>
+
+				<GridCol span={{ base: 12, lg: 6 }}>
+					<AttackHistory />
+				</GridCol>
+			</Grid>
+		</Container>
+	);
+};
 
 export default AttackLog;
