fd4be187060e9ba4602715a82c03e28344c4c58f
omegamaster86 <masya868686@gmail.com>
2025-07-12 16:04:28 +0900

    AttackLogコンポーネントを更新し、searchParamsから選択された会社の情報を取得して、CompanyInformation、PersonInfo、AttackLogForm、AttackHistoryコンポーネントにpropsとして渡すようにしました。

--- app/attacklog/page.tsx
+++ app/attacklog/page.tsx
@@ -1,15 +1,27 @@
 import { Container, Grid, GridCol, Stack } from "@mantine/core";
+import { createClient } from "@/lib/supabase/server";
 import { AttackHistory } from "./attack-history";
 import { AttackLogForm } from "./attack-log-form";
 import { CompanyInformation } from "./company-info";
 import { Header } from "./header";
 import { PersonInfo } from "./person-info";
-import { createClient } from "@/lib/supabase/server";
+import type { CompanyInfo } from "@/app/types";
 
-const AttackLog = async () => {
+interface AttackLogProps {
+	searchParams: { company_id?: string };
+}
+
+const AttackLog = async ({ searchParams }: AttackLogProps) => {
 	const supabase = await createClient();
 	const { data: callingResult } = await supabase.rpc("get_calling_result", {});
 	const { data: companies } = await supabase.rpc("get_company_information", {});
+
+	const selectedCompanyId = searchParams.company_id
+		? parseInt(searchParams.company_id)
+		: null;
+	const selectedCompany = selectedCompanyId
+		? companies?.find((company: CompanyInfo) => company.company_id === selectedCompanyId)
+		: null;
 
 	return (
 		<Container size="xl" py="lg">
@@ -18,14 +30,19 @@ const AttackLog = async () => {
 			<Grid mt="xl" gutter="lg">
 				<GridCol span={{ base: 12, lg: 6 }}>
 					<Stack gap="lg">
-						<CompanyInformation companies={companies} />
-						<PersonInfo />
-						<AttackLogForm callingResult={callingResult || []} />
+						<CompanyInformation
+							selectedCompany={selectedCompany}
+						/>
+						<PersonInfo selectedCompany={selectedCompany} />
+						<AttackLogForm
+							callingResult={callingResult || []}
+							selectedCompany={selectedCompany}
+						/>
 					</Stack>
 				</GridCol>
 
 				<GridCol span={{ base: 12, lg: 6 }}>
-					<AttackHistory />
+					<AttackHistory selectedCompany={selectedCompany} />
 				</GridCol>
 			</Grid>
 		</Container>
