1b0d3652c87e7fa0c72d4a632c27664326b44bbd
omegamaster86 <masya868686@gmail.com>
2025-07-12 14:50:53 +0900

    AttackLogのヘッダーコンポーネントをリファクタリングし、MantineのGroupとTitleを使用してデザインを改善しました。また、ボタンのスタイルを変更しました。

--- app/attacklog/header.tsx
+++ app/attacklog/header.tsx
@@ -1,16 +1,33 @@
-
-import { Button} from '@mantine/core';
-import { IconChevronsLeft, IconChevronsRight, IconX } from '@tabler/icons-react';
+import { Button, Group, Title } from "@mantine/core";
+import {
+	IconChevronsLeft,
+	IconChevronsRight,
+	IconX,
+} from "@tabler/icons-react";
 
 export const Header = () => {
-  return (
-    <div className="flex justify-between items-center">
-      <p>AttackLog</p>
-      <div className="flex gap-4">
-        <Button leftSection={<IconChevronsLeft size={18} />}>前</Button>
-        <Button leftSection={<IconChevronsRight size={18} />}>次</Button>
-        <Button leftSection={<IconX size={18} />}>閉</Button>
-      </div>
-    </div>
-  )
-}
+	return (
+		<Group justify="space-between" align="center">
+			<Title order={2}>アタックログ</Title>
+			<Group gap="sm">
+				<Button
+					leftSection={<IconChevronsLeft size={18} />}
+					variant="light"
+					color="green"
+				>
+					前
+				</Button>
+				<Button
+					leftSection={<IconChevronsRight size={18} />}
+					variant="light"
+					color="green"
+				>
+					次
+				</Button>
+				<Button leftSection={<IconX size={18} />} variant="light" color="green">
+					閉
+				</Button>
+			</Group>
+		</Group>
+	);
+};
