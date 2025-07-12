ece0626b93e4a8f014ea24a3c2c26286777d0875
omegamaster86 <masya868686@gmail.com>
2025-07-12 14:30:13 +0900

    新しいインターフェースCallingResultItemとCompanyInfoをapp/types/index.tsxに追加しました。

---
+++ app/types/index.tsx
@@ -0,0 +1,23 @@
+export interface CallingResultItem {
+  id: number;
+  name: string;
+}
+
+export interface CallingResultItem {
+  id: number;
+  name: string;
+}
+
+export interface CompanyInfo {
+  company_id: number;
+  company_name: string;
+  address: string;
+  company_website: string;
+  registered_phone_number: string;
+  department_name: string;
+  key_person_name: string;
+  key_person_position: string;
+  key_person_phone_number: string;
+  key_person_email: string;
+  key_person_note: string;
+}
